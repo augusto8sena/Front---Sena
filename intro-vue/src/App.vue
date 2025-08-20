6c993e7059351ce598623556673c71b86b349062
Augusto Sena <augusto.sena6@aluno.senai.br>
2025-08-20 08:46:10 -0300

    dgsgsr

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
-import Exemplo01 from './components/exerc/Exemplo01Interpolacao.vue'
+import Exemplo01 from './components/exemplos/Exemplo01Interpolacao.vue'
 import exerc01 from './components/exerc/exerc01.vue';
 import exerc02 from './components/exerc/exerc02.vue';
 import exerc03 from './components/exerc/exerc03.vue';
-import Exemplo02 from './components/exerc/Exemplo02Condicional.vue';
+import Exemplo02 from './components/exemplos/Exemplo02Condicional.vue';
 import exerc04 from './components/exerc/exerc04.vue';
 import exerc05 from './components/exerc/exerc05.vue';
-import Exemplo03 from './components/exerc/Exemplo03Lista.vue';
+import Exemplo03 from './components/exemplos/Exemplo03Lista.vue';
 import exerc06 from './components/exerc/exerc06.vue';
 import exerc07 from './components/exerc/exerc07.vue';
-import Exemplo05 from './components/exerc/Exemplo05mode.vue';
+import Exemplo05 from './components/exemplos/Exemplo05mode.vue';
 import exerc08 from './components/exerc/exerc08.vue';
 import exerc09 from './components/exerc/exerc09.vue';
 import exerc10 from './components/exerc/exerc10.vue';
-import Exemplo06 from './components/exerc/Exemplo06Classe.vue';
+import Exemplo06 from './components/exemplos/Exemplo06Classe.vue';
 import exerc11 from './components/exerc/exerc11.vue';
 </script>
 
