b1b55045b23607da3e6c9e1ab534b6ae536ebafc
Augusto Sena <augusto.sena6@aluno.senai.br>
2025-08-20 07:59:20 -0300

    fsbf

---
+++ intro-vue/src/components/exerc/Exemplo06Classe.vue
@@ -0,0 +1,18 @@
+<script setup>
+    let validacao = true;
+</script>
+
+<template>
+    <p :class="validacao ?'ok':'falha'"> Utilizando a diretiva class</p>
+</template>
+
+<style scoped>
+
+.ok{
+    color: green;
+}
+
+.falha{
+    color: red;
+}
+</style>
